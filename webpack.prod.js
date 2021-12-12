c2fbfe6590cbff63c7c5aa3c543142738d7a89bf
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-12-12 19:33:22 +0300

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,9 @@
 const { merge } = require('webpack-merge');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common');
+
 // const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
