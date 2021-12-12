33e0395b1b5eb73db49b1feb9c991338757b8e63
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-12-12 19:45:32 +0300

    fix 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,27 @@
+/* eslint-disable linebreak-style */
 function genPosition() {
-    return Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+  return Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+}
+
+const parentDiv = document.getElementsByClassName('wrapper')[0];
+const img = document.images[0];
+img.parentElement.removeChild(img);
+
+for (let i = 0; i < 4 * 4; i += 1) {
+  const div = document.createElement('div');
+  parentDiv.appendChild(div);
+}
+
+function setActive() {
+  if (document.images[0]) {
+    const activeDiv = document.images[0].parentElement;
+    activeDiv.removeChild(img);
   }
-  
-  const parentDiv = document.getElementsByClassName('wrapper')[0];
-  const img = document.images[0];
-  img.parentElement.removeChild(img);
-  
-  for (let i = 0; i < 4 * 4; i += 1) {
-    const div = document.createElement('div');
-    parentDiv.appendChild(div);
-  }
-  
-  function setActive() {
-    if (document.images[0]) {
-      const activeDiv = document.images[0].parentElement;
-      activeDiv.removeChild(img);
-    }
-  
-    const childDivs = parentDiv.children;
-    const index = genPosition();
-    childDivs[index].appendChild(img);
-  }
-  
-  const timerId = setInterval(setActive, 500);
-  setTimeout(() => { clearInterval(timerId); }, 10000);
+
+  const childDivs = parentDiv.children;
+  const index = genPosition();
+  childDivs[index].appendChild(img);
+}
+
+const timerId = setInterval(setActive, 500);
+setTimeout(() => { clearInterval(timerId); }, 10000);
